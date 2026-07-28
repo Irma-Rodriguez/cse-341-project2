556134575a43a0529aea70c15aa72a99e01e09ee
Irma-Rodriguez <irodriguezcardenas@byupathway.edu>
2026-07-28 13:05:16 -0500

    Changing authentication middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,8 +1,4 @@
 const isAuthenticated = (req, res, next) => {
-    console.log("=== AUTH MIDDLEWARE ===");
-    console.log("Session:", req.session);
-    console.log("User:", req.session.user);
-
     if (req.session.user === undefined) {
         return res.status(401).json("You do not have access.");
     }
