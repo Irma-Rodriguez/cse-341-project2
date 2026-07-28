542785cef4cef16b0171fa7937d9ce7e415d1c94
Irma-Rodriguez <irodriguezcardenas@byupathway.edu>
2026-07-28 12:44:01 -0500

    Test authentication middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,15 @@
 const isAuthenticated = (req, res, next) => {
+    console.log("=== AUTH MIDDLEWARE ===");
+    console.log("Session:", req.session);
+    console.log("User:", req.session.user);
+
     if (req.session.user === undefined) {
         return res.status(401).json("You do not have access.");
     }
+
     next();
 };
 
 module.exports = {
     isAuthenticated
-}
+};
